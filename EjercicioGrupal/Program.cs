6f990973b5b8d81cd1c44624e009a7e1b3b48626
Fernanda Mishell Monzón Rojas <mishellrojas2016@gmail.com>
2025-08-03 22:04:22 -0600

    Add project files.

---
+++ EjercicioGrupal/Program.cs
@@ -0,0 +1,33 @@
+﻿using EjercicioGrupal;
+using System;
+
+SalaComun Sala1 = new SalaComun("Sala 103", "Planta Baja");
+Sala1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(3)), "Fernanda", 23);
+Sala1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(3)), "Jose", 15);
+Sala1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "Jeremy", 51);
+
+Auditorio Auditiorio1 = new Auditorio("Simon Bolivar", "Sección Oeste", false);
+Auditiorio1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(2)), "Maria", 80);
+Auditiorio1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(2)), "Jessica", 30);
+Auditiorio1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "Pablo", 100);
+Auditorio Auditiorio2 = new Auditorio("Miguel de Cervantes", "Sección Norte", true);
+Auditiorio2.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(2)), "Mishell", 75);
+Auditiorio2.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(2)), "Ana", 60);
+Auditiorio2.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "Paola", 90);
+
+Laboratorio Laboratorio1 = new Laboratorio("Laboratorio de Programación", "Planta Alta", true, false);
+Laboratorio1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Carlos", 15);
+Laboratorio1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Elmer", 19);
+Laboratorio1.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "David", 25);
+Laboratorio Laboratorio2 = new Laboratorio("Laboratorio de Quimica", "Planta Alta", false, true);
+Laboratorio2.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Gabriela", 9);
+Laboratorio2.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Rossmery", 13);
+Laboratorio2.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Alexis", 30);
+Laboratorio Laboratorio3 = new Laboratorio("Laboratorio de Programación", "Planta Alta", true, true);
+Laboratorio3.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Diego", 10);
+Laboratorio3.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Paolo", 15);
+Laboratorio3.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Sebastian", 25);
+Laboratorio Laboratorio4 = new Laboratorio("Laboratorio de clases", "Planta Alta", false, false);
+Laboratorio4.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Giovanna", 10);
+Laboratorio4.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Sandra", 15);
+Laboratorio4.Reservar(DateOnly.FromDateTime(DateTime.Now.AddDays(10)), "Antonia", 25);
---
+++ EjercicioGrupal/Sala.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGrupal
+{
+    public abstract class Sala
+    {
+        public string Nombre;
+        public int Capacidad;
+        public string Ubicacion;
+        public bool Reservada = false;
+        public DateOnly? FechaReservada = null;
+
+        public Sala(string nombre, int capacidad, string ubicacion)
+        {
+            Nombre = nombre;
+            Capacidad = capacidad;
+            Ubicacion = ubicacion;
+        }
+        public abstract void Reservar(DateOnly fecha, string organizador, int capacidadRequerida);
+    }
+
+    public class SalaComun : Sala
+    {
+        public SalaComun(string nombre, string ubicacion) : base(nombre, 50, ubicacion)
+        {
+        }
+        public override void Reservar(DateOnly fecha, string organizador, int capacidadRequerida)
+        {
+            if (FechaReservada.HasValue && FechaReservada.Value < DateOnly.FromDateTime(DateTime.Now))
+            {
+                Reservada = false;
+                FechaReservada = null;
+                return;
+            }
+
+            if (capacidadRequerida > Capacidad)
+            {
+                Console.WriteLine($"La sala {Nombre} solo tiene capacidad para {Capacidad} personas. \n");
+                return;
+            }
+
+            if (Reservada && FechaReservada == fecha)
+            {
+                Console.WriteLine($"La sala {Nombre} ya esta reservada para la fecha {FechaReservada.Value}. \n");
+                return;
+            }
+
+            if (fecha < DateOnly.FromDateTime(DateTime.Now))
+            {
+                Console.WriteLine($"La fecha {fecha} no es valida, debe ser una fecha futura. \n");
+                return;
+            }
+
+            Reservada = true;
+            FechaReservada = fecha;
+            Console.WriteLine($"La sala {Nombre} ha sido reservada para la fecha {fecha} por {organizador}. \n");
+        }
+    }
+
+    public class Auditorio : Sala
+    {
+        public bool TienePantalla;
+
+        public Auditorio(string nombre, string ubicacion, bool tienePantalla) : base(nombre, 80, ubicacion)
+        {
+            TienePantalla = tienePantalla;
+        }
+
+        public override void Reservar(DateOnly fecha, string organizador, int capacidadRequerida)
+        {
+            if (FechaReservada.HasValue && FechaReservada.Value < DateOnly.FromDateTime(DateTime.Now))
+            {
+                Reservada = false;
+                FechaReservada = null;
+                return;
+            }
+
+            if (capacidadRequerida > Capacidad)
+            {
+                Console.WriteLine($"El auditorio {Nombre} solo tiene capacidad para {Capacidad} personas. \n");
+                return;
+            }
+
+                if (Reservada && FechaReservada == fecha)
+                {
+                    Console.WriteLine($"El auditorio {Nombre} ya esta reservado para la fecha {FechaReservada.Value}. \n");
+                    return;
+                }
+            if (fecha < DateOnly.FromDateTime(DateTime.Now))
+            {
+                Console.WriteLine($"La fecha {fecha} no es valida, debe ser una fecha futura. \n");
+                return;
+            }
+
+            if (TienePantalla)
+            {
+                Console.WriteLine($"El auditorio {Nombre} cuenta con pantalla para presentaciones.");
+            }
+            Reservada = true;
+            FechaReservada = fecha;
+            Console.WriteLine($"El auditorio {Nombre} ha sido reservado para la fecha {fecha} por {organizador}. \n");
+        }
+    }
+
+        public class Laboratorio : Sala
+        {
+            bool TieneComputadoras;
+            bool TieneProyector;
+            public Laboratorio(string nombre, string ubicacion, bool tieneComputadoras, bool tieneProyector) : base(nombre, 20, ubicacion)
+            {
+                TieneComputadoras = tieneComputadoras;
+                TieneProyector = tieneProyector;
+            }
+
+            public override void Reservar(DateOnly fecha, string organizador, int capacidadRequerida)
+            {
+                if (FechaReservada.HasValue && FechaReservada.Value < DateOnly.FromDateTime(DateTime.Now))
+                {
+                    Reservada = false;
+                    FechaReservada = null;
+                    return;
+                }
+
+        if (capacidadRequerida > Capacidad)
+        {
+            Console.WriteLine($"El laboratorio {Nombre} solo tiene capacidad para {Capacidad} personas. \n");
+            return;
+        }
+                    if (Reservada && FechaReservada == fecha)
+                {
+                    Console.WriteLine($"El laboratorio {Nombre} ya esta reservado para la fecha {FechaReservada.Value}. \n");
+                    return;
+                }
+
+                  if (fecha < DateOnly.FromDateTime(DateTime.Now))
+                {
+                    Console.WriteLine($"El laboratorio {fecha} no es valida, debe ser una fecha futura. \n");
+                    return;
+                }
+                if (TieneComputadoras && TieneProyector)
+                {
+                    Console.WriteLine($"El laboratorio {Nombre} cuenta con computadoras y proyector para uso de los estudiantes.");
+                }
+                else if (TieneComputadoras)
+                {
+                    Console.WriteLine($"El laboratorio {Nombre} cuenta con computadoras para uso de los estudiantes.");
+                }
+                else if (TieneProyector)
+                {
+                    Console.WriteLine($"El laboratorio {Nombre} cuenta con proyector.");
+                }
+                else 
+                {
+                    Console.WriteLine($"El laboratorio {Nombre} no cuenta con computadoras ni proyector.");
+                }
+
+                    Reservada = true;
+                FechaReservada = fecha;
+                Console.WriteLine($"El laboratorio {Nombre} ha sido reservado para la fecha {fecha} por {organizador}. \n");
+            }
+        }
+}
